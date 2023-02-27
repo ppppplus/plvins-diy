ba52683d355a3ab5e06cee18c26bdd4d567ffa49
tjh <ppppplustjh@gmail.com>
2023-02-28 01:49:09 +0800

    default success

---
+++ PL-VINS/feature_tracker/src/line_descriptor/build/CMakeFiles/linedesc.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nvidia/catkin_plvins/src/PL-VINS/feature_tracker/src/line_descriptor/build/CMakeFiles/3.16.3/CompilerIdCXX/CMakeCXXCompilerId.cpp" "/home/nvidia/catkin_plvins/src/PL-VINS/feature_tracker/src/line_descriptor/build/CMakeFiles/linedesc.dir/CMakeFiles/3.16.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "/home/nvidia/catkin_plvins/src/PL-VINS/feature_tracker/src/line_descriptor/src/LSDDetector_custom.cpp" "/home/nvidia/catkin_plvins/src/PL-VINS/feature_tracker/src/line_descriptor/build/CMakeFiles/linedesc.dir/src/LSDDetector_custom.cpp.o"
+  "/home/nvidia/catkin_plvins/src/PL-VINS/feature_tracker/src/line_descriptor/src/binary_descriptor_custom.cpp" "/home/nvidia/catkin_plvins/src/PL-VINS/feature_tracker/src/line_descriptor/build/CMakeFiles/linedesc.dir/src/binary_descriptor_custom.cpp.o"
+  "/home/nvidia/catkin_plvins/src/PL-VINS/feature_tracker/src/line_descriptor/src/binary_descriptor_matcher.cpp" "/home/nvidia/catkin_plvins/src/PL-VINS/feature_tracker/src/line_descriptor/build/CMakeFiles/linedesc.dir/src/binary_descriptor_matcher.cpp.o"
+  "/home/nvidia/catkin_plvins/src/PL-VINS/feature_tracker/src/line_descriptor/src/draw_custom.cpp" "/home/nvidia/catkin_plvins/src/PL-VINS/feature_tracker/src/line_descriptor/build/CMakeFiles/linedesc.dir/src/draw_custom.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "linedesc_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/nvidia/package/opencv-3.2.0/build"
+  "/home/nvidia/package/opencv-3.2.0/include"
+  "/home/nvidia/package/opencv-3.2.0/include/opencv"
+  "/home/nvidia/package/opencv-3.2.0/modules/core/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/flann/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/imgproc/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/ml/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/photo/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/video/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/viz/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/imgcodecs/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/shape/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/videoio/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/highgui/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/objdetect/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/superres/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/ts/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/features2d/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/calib3d/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/stitching/include"
+  "/home/nvidia/package/opencv-3.2.0/modules/videostab/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PL-VINS/feature_tracker/src/line_descriptor/build/CMakeFiles/linedesc.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../lib/liblinedesc.pdb"
+  "../lib/liblinedesc.so"
+  "CMakeFiles/linedesc.dir/CMakeFiles/3.16.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "CMakeFiles/linedesc.dir/src/LSDDetector_custom.cpp.o"
+  "CMakeFiles/linedesc.dir/src/binary_descriptor_custom.cpp.o"
+  "CMakeFiles/linedesc.dir/src/binary_descriptor_matcher.cpp.o"
+  "CMakeFiles/linedesc.dir/src/draw_custom.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linedesc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
